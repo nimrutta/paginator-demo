0b203344476a83e740c68dbb11142ad447ebcc49
Walter Kimaro <wakyj07@gmail.com>
2020-02-28 15:01:09 +0300

    Show table example in Examples component

--- src/examples/App.js
+++ src/examples/App.js
@@ -3,8 +3,15 @@ import IPFPaginator from "../lib";
 
 const App = () => (
   <div style={{ width: 640, margin: "15px auto" }}>
-    <h1>Users list</h1>
+    <h1>Billionaires list</h1>
     <IPFPaginator data={["Sundar Pichai", "Satya Nadella", "Jeff Bezos"]} currentPage={5} totalCount={80} />
+
+    <br /> <br />
+    <h1>Users Table</h1>
+    <IPFPaginator
+      asTable 
+      data={[{name: "Walter", age: "26"}, {name: "James", age: "12"}]} 
+    />
   </div>
 );
 
