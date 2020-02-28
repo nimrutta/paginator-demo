6eb346d576f8c51891c4073afe573c4afa03063e
Walter Kimaro <wakyj07@gmail.com>
2020-02-28 14:59:49 +0300

    Add logic for rendering table in iPFPaginator

--- src/lib/IPFPaginator.jsx
+++ src/lib/IPFPaginator.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import Paginator from "./components/Paginator";
 
-function IPFPaginator({data, dataType = 'data', renderItem, ...paginatorProps}){
+function validateData(data) {
+    return !data.some(item => typeof item !== "object");
+}
+
+function IPFPaginator({data, dataType = 'data', asTable, renderItem, className, ...paginatorProps}){
     function renderDataItem(item, index){
         if(typeof renderItem === 'function')
             return renderItem(item, index);
@@ -18,9 +22,69 @@ function IPFPaginator({data, dataType = 'data', renderItem, ...paginatorProps}){
         return data.map((item, index) => renderDataItem(item, index))
     }
 
+    function renderTableRow(rowData, index){
+        if(typeof renderItem === 'function')
+            return renderItem(rowData, index);
+        else
+            return (
+                <tr key={index}>
+                    {
+                        Object.entries(rowData).map(([key, value]) => (
+                            <td key={`td${key}`}>{value}</td>
+                        ))
+                    }
+                </tr>
+            );
+    }
+    
+    function renderTableBody() {
+        return (
+            <tbody>
+                { data.length < 1 && 
+                    ( <tr><td>No {dataType} found.</td></tr> ) 
+                }
+
+                {
+                    data.map((item, index) => renderTableRow(item, index))
+                }
+            </tbody>
+        );
+    }
+    
+    function renderTableHead() {
+        return (
+            <thead>
+                <tr>
+                    {
+                        Object.entries(data[0]).map(([key]) => (
+                            <th key={`th${key}`}>{key}</th>
+                        ))
+                    }
+                </tr>
+            </thead>
+        );
+    }
+    
+    function renderTable() {
+        if(!data) return null;
+
+        if(!validateData(data)){
+            throw(new Error('Some error!'));
+        }
+
+        return (
+            <table className={className}>
+                { renderTableHead() }
+                { renderTableBody() }
+            </table>
+        );
+    }
+
     return (
         <React.Fragment>
-            { renderDataItems() }
+            { !asTable && renderDataItems() }
+
+            { asTable && renderTable() }
 
             <Paginator {...paginatorProps} />
         </React.Fragment>
