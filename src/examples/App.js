021cbc62134ce64d591abd313c6f37f2f060563f
Walter Kimaro <wakyj07@gmail.com>
2020-03-06 13:32:44 +0300

    Add example for rendering custom table row

--- src/examples/App.js
+++ src/examples/App.js
@@ -1,6 +1,26 @@
 import React from "react";
 import IPFPaginator from "../lib";
 
+const renderCustomRow = (rowData, index) => (
+  <tr key={index}>
+    {
+      Object.entries(rowData).map(([key, value]) => {
+        if(key == 'skills')
+          return (
+            <td key={`td${key}`}>
+              <ul> { value.map((skill, index) => 
+                  <li key={index}>{skill}</li>
+                )}
+              </ul>
+            </td>
+          );
+
+        return <td key={`td${key}`}>{value}</td>
+      })
+    }
+  </tr>
+);
+
 const App = () => (
   <div style={{ width: 640, margin: "15px auto" }}>
     <h1>Billionaires list</h1>
@@ -11,6 +31,14 @@ const App = () => (
     <IPFPaginator
       asTable 
       data={[{name: "Walter", age: "26"}, {name: "James", age: "12"}]} 
+    />
+
+    <br /> <br />
+    <h1>User Skills Table</h1>
+    <IPFPaginator
+      asTable 
+      data = {[{name: "Walter", skills: ["Eating", "Arguing"]}, {name: "James", skills: ["Fishing"]}]}
+      renderCustomRow = {renderCustomRow}
     />
   </div>
 );
